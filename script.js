2ce7f5ea9ceee28e4493ed01ed5fe2fcd0869213
Anamika <anamikadobriyal21@gmail.com>
2024-10-12 01:02:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 // Complete the js code
-function Car(make, model) {}
+//car constructor function
+function Car(make, model) {
+	 this.make = make;
+  this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+
+// Add getMakeModel method to Car prototype
+Car.prototype.getMakeModel = function () {
+  return `${this.make} ${this.model}`;
+};
+
+//sportsCar Constructor function
+function SportsCar(make, model, topSpeed) {
+	// Call the Car constructor to inherit make and model
+  Car.call(this, make, model);
+  
+  // Add topSpeed property specific to SportsCar
+  this.topSpeed = topSpeed;
+}
+
+// Inherit the Car prototype in SportsCar prototype
+SportsCar.prototype = Object.create(Car.prototype);
+
+// Add getTopSpeed method to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function () {
+  return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
