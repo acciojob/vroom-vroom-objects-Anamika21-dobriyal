aa8a51b2a60e7c8a60288aef0015ea19ad3d1e1f
Anamika <anamikadobriyal21@gmail.com>
2024-10-12 01:18:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -28,6 +28,12 @@ SportsCar.prototype.getTopSpeed = function () {
   return this.topSpeed;
 };
 
+
+// Example usage:
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel()); // Output: Ferrari Testarossa
+console.log(car.getTopSpeed()); // Output: 200
+
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
